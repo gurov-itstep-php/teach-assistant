801abdca70f412f41f74a5fc85dafc6e56c40571
Anatolii <gurov.anatoliy@gmail.com>
2021-08-10 11:05:59 +0300

    Добавлены классы Provider и Model

--- index.php
+++ index.php
@@ -7,10 +7,25 @@ require_once('sys/lib/autoloader.php');
 // Тесты базовых и ключевых классов:
 // -------------------------------------
 /*
+$provider = new \sys\lib\Provider;
+$model = new \sys\core\Model;
+$model->test();
+
+$result = $model->execute_select_query('select * from roles');
+echo('<pre>');
+print_r($result);
+echo('</pre>');
+*/
+
+//$model->execute_dml_query("insert into roles (name) values (?)", ['moder']);
+//$model->execute_dml_query("insert into roles (name) values (?)", ['user']);
+//$model->execute_dml_query("insert into roles (name) values ('admin')"); // регистрируем роль админ
+
+/*
 $testObject = new \sys\core\Test;
 $baseController = new \sys\core\Controller;
 */
-$homeController = new \app\controllers\Home;
-$homeController->index();
+//$homeController = new \app\controllers\Home;
+//$homeController->index();
 //$homeController->about();
 //$homeController->contact();
