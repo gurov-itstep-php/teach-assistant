43dc60ebcd360de51cf410bdefe3c4a0c0369db0
Anatolii <gurov.anatoliy@gmail.com>
2021-08-10 12:24:47 +0300

    Добавлена система маршрутизации

---
+++ app/controllers/errors.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+use \sys\core\View as View;
+use \sys\core\Controller as Controller;
+
+class Errors extends Controller {
+    public function notfound() {
+        return new View('errors/notfound.php');
+    }
+}
---
+++ sys/config/segments.php
@@ -0,0 +1,3 @@
+<?php
+
+define('ADDITIONAL_SEGMENTS', 1); // кол-во сегментов в адресе localhost/php/
