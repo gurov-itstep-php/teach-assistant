48c3ded8c344c3124296324342b317a1b5314c08
Anatolii <gurov.anatoliy@gmail.com>
2021-08-20 11:36:49 +0300

    Добавлен тег генератора форм на стр. contact

--- app/views/home/contact.php
+++ app/views/home/contact.php
@@ -1,3 +1,8 @@
 <div class="main-box">
     <h3>Контакты</h3>
+    <hr>
+    <p>Связаться с администрацией сайта</p>
+    <div class="form-box">
+        <?php $form->generate(); ?>
+    </div>
 </div>
